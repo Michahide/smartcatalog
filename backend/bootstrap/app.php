1f470680722a9e114a3993a589f0c8bbf2f30823
Michahide <michaelruswan@gmail.com>
2026-05-22 16:51:13 +0700

    refactor: reorganize routing configuration in app bootstrap file

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -6,14 +6,15 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        api: __DIR__ . '/../routes/api.php',
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
         apiPrefix: 'api',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->statefulApi();
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
+    ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->create();
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
